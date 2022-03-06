b8781d78fe3b36933b09a1150e88da6a7d368ed6
Cutset <const.testu@icloud.com>
2022-03-06 14:35:12 +0100

    Feature: Adding sandbox fetching function for ADRESSE

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
 const montlimart = require("./sources/montlimart");
+const adresse = require("./sources/adresse")
 
 // async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
 
@@ -17,11 +18,34 @@ const montlimart = require("./sources/montlimart");
 //   }
 // }
 
-async function sandboxMontlimart (eshop = 'https://www.montlimart.com/toute-la-collection.html') {
+/**
+ * Scrapes and fetches the products of the Montlimart eshop
+ * @param {url for the website} eshop 
+ */
+// async function sandboxMontlimart (eshop = 'https://www.montlimart.com/toute-la-collection.html') {
+
+//   try {
+//     console.log(`🕵️‍♀️  browsing ${eshop} source`);
+//     const products = await montlimart.scrape(eshop);
+
+//     console.log(products);
+//     console.log('done');
+//     process.exit(0);
+//   } catch (e) {
+//     console.error(e);
+//     process.exit(1);
+//   }
+// }
+
+/**
+ * Scrapes and fetches the products of the ADRESSE Paris eshop
+ * @param {url for the eshop's website} eshop 
+ */
+async function sandboxAdresse (eshop = 'https://adresse.paris/630-toute-la-collection') {
 
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
-    const products = await montlimart.scrape(eshop);
+    const products = await adresse.scrape(eshop);
 
     console.log(products);
     console.log('done');
@@ -36,4 +60,5 @@ async function sandboxMontlimart (eshop = 'https://www.montlimart.com/toute-la-c
 const [,, eshop] = process.argv;
 
 //sandbox(eshop);
-sandboxMontlimart(eshop);
+//sandboxMontlimart(eshop);
+sandboxAdresse(eshop)
