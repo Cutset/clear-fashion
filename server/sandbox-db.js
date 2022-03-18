7f04722ca27b4da0ca45a29145917d0229620fe3
Cutset <const.testu@icloud.com>
2022-03-18 23:38:41 +0100

    Cleaning useless code bits

--- server/sandbox-db.js
+++ server/sandbox-db.js
@@ -5,82 +5,7 @@ const db = require('./db');
 const montlimart = require("./sources/montlimart");
 const adresse = require("./sources/adresse")
 
-async function sandbox () {
-  try {
-    let products = [];
-    let pages = [
-      'https://www.dedicatedbrand.com/en/men/basics',
-      'https://www.dedicatedbrand.com/en/men/sale'
-    ];
-
-    console.log(`🕵️‍♀️  browsing ${pages.length} pages with for...of`);
-
-    // Way 1 with for of: we scrape page by page
-    for (let page of pages) {
-      console.log(`🕵️‍♀️  scraping ${page}`);
-
-      let results = await dedicatedbrand.scrape(page);
-
-      console.log(`👕 ${results.length} products found`);
-
-      products.push(results);
-    }
-
-    pages = [
-      'https://www.loom.fr/collections/hauts-homme',
-      'https://www.loom.fr/collections/bas-homme'
-    ];
-
-    console.log('\n');
-
-    console.log(`🕵️‍♀️  browsing ${pages.length} pages with Promise.all`);
-
-    const promises = pages.map(page => loom.scrape(page));
-    const results = await Promise.all(promises);
-
-    console.log(`👕 ${results.length} results of promises found`);
-    console.log(`👕 ${results.flat().length} products found`);
-
-    console.log(results);
-    console.log(results.flat());
-
-    products.push(results.flat());
-    products = products.flat();
-
-    console.log('\n');
-
-    console.log(`👕 ${products.length} total of products found`);
-
-    console.log('\n');
-
-    const result = await db.insert(products);
-
-    console.log(`💽  ${result.insertedCount} inserted products`);
-
-    console.log('\n');
-
-    console.log('💽  Find Loom products only');
-
-    const loomOnly = await db.find({'brand': 'loom'});
-
-    console.log(`👕 ${loomOnly.length} total of products found for Loom`);
-    console.log(loomOnly);
-    
-    db.close();
-  } catch (e) {
-    console.error(e);
-  }
-}
-
-async function scraper(eshop_scraper,url){
-    console.log(`🕵️‍♀️  scraping ${url}`);
-    let results = await eshop_scraper.scrape(url);
-
-    console.log(`👕 ${results.length} products found`);
-    return results;
-}
-
-async function sandbox2 () {
+async function sandbox() {
   try {
     let products = [];
 
@@ -119,8 +44,6 @@ async function sandbox2 () {
       }
     }
 
-  
-
     products = products.flat();
     console.log(`\n👕 ${products.length} total of products found\n`);
     
@@ -134,5 +57,5 @@ async function sandbox2 () {
   }
 }
 
-sandbox2();
+sandbox();
 //db.dropDatabase()
